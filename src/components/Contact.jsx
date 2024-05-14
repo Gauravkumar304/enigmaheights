0324d518b319efdd249d9866f8ed0be8a0d9c6df
Gauravkumar304 <pushkarkumarsingh7499@gmail.com>
2024-05-14 15:15:30 +0530

    rating is done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,10 @@
 import React,{useState} from 'react'
 import { FaWhatsapp, FaInstagram,  FaLinkedin,FaTelegram } from 'react-icons/fa';
 
+import Rating from '@mui/material/Rating';
+import Box from '@mui/material/Box';
+import StarIcon from '@mui/icons-material/Star';
+
 
 const Contact = () => {
 
@@ -22,6 +26,39 @@ const handleSubmit = (e) => {
   console.log('Email:', email);
   console.log('Feedback:', feedback);
 };
+
+
+const labels = {
+  0.5: 'Useless',
+  1: 'Useless+',
+  1.5: 'Poor',
+  2: 'Poor+',
+  2.5: 'Ok',
+  3: 'Ok+',
+  3.5: 'Good',
+  4: 'Good+',
+  4.5: 'Excellent',
+  5: 'Excellent+',
+};
+
+
+const StyledIconContainer = (props) => {
+  const { value, ...other } = props;
+  return (
+    <span {...other}>
+      <StarIcon style={{ color: value <= props.hover ? 'gold' : 'grey' }} fontSize="inherit" />
+    </span>
+  );
+};
+
+function getLabelText(value) {
+  return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+}
+
+  const [value, setValue] = React.useState(2);
+  const [hover, setHover] = React.useState(-1);
+
+
 
 return (
   <div className='cont' id='Contact' >
@@ -45,9 +82,43 @@ return (
           onChange={handleFeedbackChange}
           required
         />
+
+<Box
+      sx={{
+        width: 200,
+        display: 'flex',
+        alignItems: 'center',
+        color:'Gold',
+      }}
+    >
+      <Rating
+        name="hover-feedback"
+        value={value}
+        precision={0.5}
+        getLabelText={getLabelText}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        onChangeActive={(event, newHover) => {
+          setHover(newHover);
+        }}
+
+        IconContainerComponent={(props) => (
+          <StyledIconContainer hover={hover} {...props} />
+        )}
+
+        emptyIcon={<StarIcon style={{ opacity:0.55 }} fontSize="inherit" />}
+      />
+      {value !== null && (
+        <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+      )}
+    </Box>
         
       </div>
+
+    
       <button type="submit">Submit</button>
+
     </form>
     <div className='icon-container'>
       <p>Send feedback via:</p>
