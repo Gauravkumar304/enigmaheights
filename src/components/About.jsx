b0ae18b060ba2d8ada38757474073e5aa38fb986
Gauravkumar304 <pushkarkumarsingh7499@gmail.com>
2024-05-18 14:31:14 +0530

    font

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,12 +13,13 @@ const About = () => {
           <p>"Welcome to Enigma Heights, your digital marketing partner. We specialize in unlocking the mysteries of online success for businesses worldwide. With a blend of creativity, strategy, and data-driven solutions, we elevate your brand to new heights in the digital landscape. Let's embark on a journey of discovery and growth together."</p>
           </div>
                 </div>
-       
+        
 
        
         
           < div className="abt-contain">
           <img src={about} alt="About us " />
+
           <div className="abth">
             <h2>Innovative Approach</h2>
             <p>
@@ -27,7 +28,7 @@ const About = () => {
           </div>
           <div className="abth">
             <h2>Proven Results</h2>
-            <p> our work has helped business of all sizes their goals</p>
+            <p> Our work has helped business of all sizes their goals</p>
           </div>
           {/* <div className="abt-contain"> */}
           <div className="abth">
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,142 +1,277 @@
-import React,{useState} from 'react'
-import { FaWhatsapp, FaInstagram,  FaLinkedin,FaTelegram } from 'react-icons/fa';
+// import React,{useState} from 'react'
+// import { FaWhatsapp, FaInstagram,  FaLinkedin,FaTelegram } from 'react-icons/fa';
 
+// import Rating from '@mui/material/Rating';
+// import Box from '@mui/material/Box';
+// import StarIcon from '@mui/icons-material/Star';
+
+
+// const Contact = () => {
+
+
+// const [email, setEmail] = useState('');
+// const [feedback, setFeedback] = useState('');
+
+// const handleEmailChange = (e) => {
+//   setEmail(e.target.value);
+// };
+
+// const handleFeedbackChange = (e) => {
+//   setFeedback(e.target.value);
+// };
+
+// const handleSubmit = (e) => {
+//   e.preventDefault();
+//   // You can implement your feedback submission logic here
+//   console.log('Email:', email);
+//   console.log('Feedback:', feedback);
+// };
+
+
+// const labels = {
+//   0.5: 'Useless',
+//   1: 'Useless+',
+//   1.5: 'Poor',
+//   2: 'Poor+',
+//   2.5: 'Ok',
+//   3: 'Ok+',
+//   3.5: 'Good',
+//   4: 'Good+',
+//   4.5: 'Excellent',
+//   5: 'Excellent+',
+// };
+
+
+// const StyledIconContainer = (props) => {
+//   const { value, ...other } = props;
+//   return (
+//     <span {...other}>
+//       <StarIcon style={{ color: value <= props.hover ? 'gold' : 'grey' }} fontSize="inherit" />
+//     </span>
+//   );
+// };
+
+// function getLabelText(value) {
+//   return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+// }
+
+//   const [value, setValue] = React.useState(2);
+//   const [hover, setHover] = React.useState(-1);
+
+
+
+// return (
+//   <div className='cont' id='Contact' >
+//     <h2 className='contact'>Feedback Form</h2>
+//     <form  action="https://formspree.io/f/mbjnbvoy"
+//   method="POST" onSubmit={handleSubmit}>
+//       <div >
+//         <label htmlFor="email">Email:</label>
+//         <textarea
+//           type="email"
+//           id="email"
+//           value={email}
+//           onChange={handleEmailChange}
+//           required
+//         />
+//       </div>
+//       <div  >
+//         <label htmlFor="feedback">Feedback:</label>
+//         <textarea
+//           id="feedback"
+//           value={feedback}
+//           onChange={handleFeedbackChange}
+//           required
+//         />
+
+// <Box
+//       sx={{
+//         width: 200,
+//         display: 'flex',
+//         alignItems: 'center',
+//         color:'Gold',
+//         padding:'15px',
+//       }}
+//     >
+//       <Rating
+//         name="hover-feedback"
+//         value={value}
+//         precision={0.5}
+//         getLabelText={getLabelText}
+//         onChange={(event, newValue) => {
+//           setValue(newValue);
+//         }}
+//         onChangeActive={(event, newHover) => {
+//           setHover(newHover);
+//         }}
+
+//         IconContainerComponent={(props) => (
+//           <StyledIconContainer hover={hover} {...props} />
+//         )}
+
+//         emptyIcon={<StarIcon style={{ opacity:0.55 }} fontSize="inherit" />}
+//       />
+//       {value !== null && (
+//         <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+//       )}
+//     </Box>
+        
+//       </div>
+    
+//       <button type="submit">Submit</button>
+
+//     </form>
+//     <div className='icon-container'>
+//       <p>Send feedback via:</p>
+//       <a href="https://wa.me/+918884491050" target="_blank" rel="noopener noreferrer">
+//         <FaWhatsapp size={32} style={{ marginRight: '10px' }} className='icon whatsapp' />
+//       </a>
+//       <a href="https://www.instagram.com/enigmaheights/" target="_blank" rel="noopener noreferrer">
+//         <FaInstagram size={32} style={{ marginRight: '10px' }} className='icon instagram' />
+//       </a>
+//       <a href="https://www.linkedin.com/in/gaurav-arya-751773305/" target="_blank" rel="noopener noreferrer">
+//        <FaLinkedin size={32} style={{ marginRight: '10px' }} className="icon linkedin"  />
+//   </a>
+//       <a href="https://t.me/gauravaryaEnigma" target="_blank" rel="noopener noreferrer">
+//        <FaTelegram size={32} style={{ marginRight: '10px' }} className="icon linkedin"  />
+//   </a>
+//     </div>
+//   </div>
+//   )
+// }
+
+// export default Contact
+
+
+import React, { useState } from 'react';
+import { FaWhatsapp, FaInstagram, FaLinkedin, FaTelegram } from 'react-icons/fa';
 import Rating from '@mui/material/Rating';
 import Box from '@mui/material/Box';
 import StarIcon from '@mui/icons-material/Star';
-
+import { useForm, ValidationError } from '@formspree/react';
 
 const Contact = () => {
+  const [email, setEmail] = useState('');
+  const [feedback, setFeedback] = useState('');
+  const [value, setValue] = useState(2);
+  const [hover, setHover] = useState(-1);
+  const [state, handleSubmit] = useForm("mbjnbvoy");
 
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-const [email, setEmail] = useState('');
-const [feedback, setFeedback] = useState('');
+  const handleFeedbackChange = (e) => {
+    setFeedback(e.target.value);
+  };
 
-const handleEmailChange = (e) => {
-  setEmail(e.target.value);
-};
+  const labels = {
+    0.5: 'Useless',
+    1: 'Useless+',
+    1.5: 'Poor',
+    2: 'Poor',
+    2.5: 'Ok',
+    3: 'Ok',
+    3.5: 'Good',
+    4: 'Good',
+    4.5: 'Excellent',
+    5: 'Excellent',
+  };
 
-const handleFeedbackChange = (e) => {
-  setFeedback(e.target.value);
-};
+  const StyledIconContainer = (props) => {
+    const { value, ...other } = props;
+    return (
+      <span {...other}>
+        <StarIcon style={{ color: value <= props.hover ? 'gold' : 'grey' }} fontSize="inherit" />
+      </span>
+    );
+  };
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  // You can implement your feedback submission logic here
-  console.log('Email:', email);
-  console.log('Feedback:', feedback);
-};
+  function getLabelText(value) {
+    return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+  }
 
+  if (state.succeeded) {
+    return <p>Thanks for your feedback!</p>;
+  }
 
-const labels = {
-  0.5: 'Useless',
-  1: 'Useless+',
-  1.5: 'Poor',
-  2: 'Poor+',
-  2.5: 'Ok',
-  3: 'Ok+',
-  3.5: 'Good',
-  4: 'Good+',
-  4.5: 'Excellent',
-  5: 'Excellent+',
-};
-
-
-const StyledIconContainer = (props) => {
-  const { value, ...other } = props;
   return (
-    <span {...other}>
-      <StarIcon style={{ color: value <= props.hover ? 'gold' : 'grey' }} fontSize="inherit" />
-    </span>
+    <div className='cont' id='Contact'>
+      <h2 className='contact'>Feedback Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <textarea
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Feedback:</label>
+          <textarea
+            id="feedback"
+            name="feedback"
+            value={feedback}
+            onChange={handleFeedbackChange}
+            required
+          />
+          <ValidationError prefix="Feedback" field="feedback" errors={state.errors} />
+        </div>
+
+        <Box
+          sx={{
+            width: 200,
+            display: 'flex',
+            alignItems: 'center',
+            color: 'Gold',
+            padding: '15px',
+          }}
+        >
+          <Rating
+            name="hover-feedback"
+            value={value}
+            precision={0.5}
+            getLabelText={getLabelText}
+            onChange={(e, newValue) => {
+              setValue(newValue);
+            }}
+            onChangeActive={(e, newHover) => {
+              setHover(newHover);
+            }}
+            IconContainerComponent={(props) => (
+              <StyledIconContainer hover={hover} {...props} />
+            )}
+            emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+          />
+          {value !== null && (
+            <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+          )}
+        </Box>
+
+        <button type="submit" disabled={state.submitting}>Submit</button>
+      </form>
+      <div className='icon-container'>
+        <p>Send feedback via:</p>
+        <a href="https://wa.me/+918884491050" target="_blank" rel="noopener noreferrer">
+          <FaWhatsapp size={32} style={{ marginRight: '10px' }} className='icon whatsapp' />
+        </a>
+        <a href="https://www.instagram.com/enigmaheights/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram size={32} style={{ marginRight: '10px' }} className='icon instagram' />
+        </a>
+        <a href="https://www.linkedin.com/in/gaurav-arya-751773305/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin size={32} style={{ marginRight: '10px' }} className="icon linkedin" />
+        </a>
+        <a href="https://t.me/gauravaryaEnigma" target="_blank" rel="noopener noreferrer">
+          <FaTelegram size={32} style={{ marginRight: '10px' }} className="icon linkedin" />
+        </a>
+      </div>
+    </div>
   );
 };
 
-function getLabelText(value) {
-  return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
-}
-
-  const [value, setValue] = React.useState(2);
-  const [hover, setHover] = React.useState(-1);
-
-
-
-return (
-  <div className='cont' id='Contact' >
-    <h2 className='contact'>Feedback Form</h2>
-    <form onSubmit={handleSubmit}>
-      <div >
-        <label htmlFor="email">Email:</label>
-        <textarea
-          type="email"
-          id="email"
-          value={email}
-          onChange={handleEmailChange}
-          required
-        />
-      </div>
-      <div  >
-        <label htmlFor="feedback">Feedback:</label>
-        <textarea
-          id="feedback"
-          value={feedback}
-          onChange={handleFeedbackChange}
-          required
-        />
-
-<Box
-      sx={{
-        width: 200,
-        display: 'flex',
-        alignItems: 'center',
-        color:'Gold',
-      }}
-    >
-      <Rating
-        name="hover-feedback"
-        value={value}
-        precision={0.5}
-        getLabelText={getLabelText}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        onChangeActive={(event, newHover) => {
-          setHover(newHover);
-        }}
-
-        IconContainerComponent={(props) => (
-          <StyledIconContainer hover={hover} {...props} />
-        )}
-
-        emptyIcon={<StarIcon style={{ opacity:0.55 }} fontSize="inherit" />}
-      />
-      {value !== null && (
-        <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
-      )}
-    </Box>
-        
-      </div>
-
-    
-      <button type="submit">Submit</button>
-
-    </form>
-    <div className='icon-container'>
-      <p>Send feedback via:</p>
-      <a href="https://wa.me/+918884491050" target="_blank" rel="noopener noreferrer">
-        <FaWhatsapp size={32} style={{ marginRight: '10px' }} className='icon whatsapp' />
-      </a>
-      <a href="https://www.instagram.com/enigmaheights/" target="_blank" rel="noopener noreferrer">
-        <FaInstagram size={32} style={{ marginRight: '10px' }} className='icon instagram' />
-      </a>
-      <a href="https://www.linkedin.com/in/gaurav-arya-751773305/" target="_blank" rel="noopener noreferrer">
-       <FaLinkedin size={32} style={{ marginRight: '10px' }} className="icon linkedin"  />
-  </a>
-      <a href="https://t.me/gauravaryaEnigma" target="_blank" rel="noopener noreferrer">
-       <FaTelegram size={32} style={{ marginRight: '10px' }} className="icon linkedin"  />
-  </a>
-    </div>
-  </div>
-  )
-}
-
-export default Contact
+export default Contact;
